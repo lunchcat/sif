49a351fd1dc0b11c3e2addb3d484501a907095ef
Sol Fisher Romanoff <sol@solfisher.com>
2023-09-02 10:27:28 +0300

    Add nix flake devshell
    TODO: build go application

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "a blazing-fast pentesting (recon/exploitation) suite";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ go ];
+        };
+      });
+}
