8f6801e0799604a814ee336dca703ca2264dbc4e
Sol Fisher Romanoff <sol@solfisher.com>
2023-09-06 15:52:43 +0300

    Add nix build instructions

--- flake.nix
+++ flake.nix
@@ -4,15 +4,33 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+
+    gomod2nix = {
+      url = "github:tweag/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.utils.follows = "utils";
+    };
   };
 
-  outputs = { self, nixpkgs, utils }:
+  outputs = { self, nixpkgs, utils, gomod2nix }:
     utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let pkgs = import nixpkgs { 
+        inherit system; 
+        overlays = [ gomod2nix.overlays.default ];
+      };
       in
       {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ go ];
+        packages.default = pkgs.buildGoApplication {
+          pname = "sif";
+          version = "0.1.0";
+          src = ./.;
+          modules = ./gomod2nix.toml;
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ 
+            go 
+            gomod2nix.packages.${system}.default
+          ];
         };
       });
 }
