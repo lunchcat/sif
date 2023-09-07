8f786508ec50036089be08ef3a34e8c71d231294
Sol Fisher Romanoff <sol@solfisher.com>
2023-09-07 22:23:27 +0300

    config: use goflags instead of pflag

--- config.go
+++ config.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/charmbracelet/log"
-	"github.com/spf13/pflag"
+	"github.com/projectdiscovery/goflags"
 )
 
 type Settings struct {
@@ -24,73 +24,77 @@ type Settings struct {
 	Timeout time.Duration
 }
 
-func parseURLs() Settings {
-	var debug = pflag.BoolP("debug", "d", false, "Enable debug logging")
-	var timeout = pflag.DurationP("timeout", "t", 10*time.Second, "General http timeout value - Default is 10 seconds")
-	var logdir = pflag.StringP("log", "l", "", "Directory to store logs")
-	var url = pflag.StringArrayP("url", "u", []string{}, "URL to check")
-	var file = pflag.StringP("file", "f", "", "File that includes URLs to check")
-	var dirlist = pflag.String("dirlist", "none", "Directory fuzzing scan size (small, medium, large)")
-	var dnslist = pflag.String("dnslist", "none", "DNS fuzzing scan size (small, medium, large)")
-	var ports = pflag.StringP("ports", "p", "none", "Scan common ports")
-	pflag.Lookup("ports").NoOptDefVal = "common"
-	var dorking = pflag.Bool("dork", false, "Enable Google dorking")
-	var noscan = pflag.Bool("noscan", false, "Do not perform base URL (robots.txt, etc) scanning")
-	var git = pflag.Bool("git", false, "Enable git repository scanning")
-	var threads = pflag.Int("threads", 10, "Number of threads to run scans on")
-	var nuclei = pflag.Bool("nuclei", false, "Scan for vulnerabilities using nuclei templates")
-	pflag.Parse()
+const (
+	Nil goflags.EnumVariable = iota
 
-	if len(*url) > 0 {
-		return Settings{
-			Debug:   *debug,
-			Timeout: *timeout,
-			Dirlist: *dirlist,
-			Dnslist: *dnslist,
-			NoScan:  *noscan,
-			URLs:    *url,
-			Dorking: *dorking,
-			Ports:   *ports,
-			LogDir:  *logdir,
-			Threads: *threads,
-			Git:     *git,
-			Nuclei:  *nuclei,
-		}
-	} else if *file != "" {
-		if _, err := os.Stat(*file); err != nil {
+	// list sizes
+	Small
+	Medium
+	Large
+
+	// port scan scopes
+	Common
+	Full
+)
+
+func parseURLs() Settings {
+	settings := &Settings{}
+
+	flagSet := goflags.NewFlagSet()
+	flagSet.SetDescription("a blazing-fast pentesting (recon/exploitation) suite")
+
+	var urls goflags.StringSlice
+	var file string
+	flagSet.CreateGroup("target", "Targets",
+		flagSet.StringSliceVarP(&urls, "urls", "u", nil, "List of URLs to check (comma-separated)", goflags.FileCommaSeparatedStringSliceOptions),
+		flagSet.StringVarP(&file, "file", "f", "", "File that includes URLs to check"),
+	)
+
+	listSizes := goflags.AllowdTypes{"small": Small, "medium": Medium, "large": Large, "none": Nil}
+	portScopes := goflags.AllowdTypes{"common": Common, "full": Full, "none": Nil}
+	flagSet.CreateGroup("scans", "Scans",
+		flagSet.EnumVar(&settings.Dirlist, "dirlist", Nil, "Directory fuzzing scan size (small/medium/large)", listSizes),
+		flagSet.EnumVar(&settings.Dnslist, "dnslist", Nil, "DNS fuzzing scan size (small/medium/large)", listSizes),
+		flagSet.EnumVar(&settings.Ports, "ports", Nil, "Port scanning scope (common/full)", portScopes),
+		flagSet.BoolVar(&settings.Dorking, "dork", false, "Enable Google dorking"),
+		flagSet.BoolVar(&settings.Git, "git", false, "Enable git repository scanning"),
+		flagSet.BoolVar(&settings.Nuclei, "nuclei", false, "Enable scanning using nuclei templates"),
+		flagSet.BoolVar(&settings.NoScan, "noscan", false, "Do not perform base URL (robots.txt, etc) scanning"),
+	)
+
+	flagSet.CreateGroup("runtime", "Runtime",
+		flagSet.BoolVarP(&settings.Debug, "debug", "d", false, "Enable debug logging"),
+		flagSet.DurationVarP(&settings.Timeout, "timeout", "t", 10*time.Second, "HTTP request timeout"),
+		flagSet.StringVarP(&settings.LogDir, "log", "l", "", "Directory to store logs in"),
+		flagSet.IntVar(&settings.Threads, "threads", 10, "Number of threads to run scans on"),
+	)
+
+	if err := flagSet.Parse(); err != nil {
+		log.Fatalf("Could not parse flags: %s", err)
+	}
+
+	if len(urls) > 0 {
+		settings.URLs = urls
+	} else if file != "" {
+		if _, err := os.Stat(file); err != nil {
 			log.Fatal(err)
 		}
-		log.Infof("Reading file %s", *file)
+		log.Infof("Reading file %s", file)
 
-		data, err := os.Open(*file)
+		data, err := os.Open(file)
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer data.Close()
 
-		var urls []string
 		scanner := bufio.NewScanner(data)
 		scanner.Split(bufio.ScanLines)
 		for scanner.Scan() {
-			urls = append(urls, scanner.Text())
+			settings.URLs = append(settings.URLs, scanner.Text())
 		}
-
-		return Settings{
-			Timeout: *timeout,
-			Debug:   *debug,
-			Dirlist: *dirlist,
-			Dnslist: *dnslist,
-			NoScan:  *noscan,
-			Dorking: *dorking,
-			Ports:   *ports,
-			URLs:    urls,
-			LogDir:  *logdir,
-			Threads: *threads,
-			Git:     *git,
-			Nuclei:  *nuclei,
-		}
+	} else {
+		log.Fatal("Please specify either a URL or a file containing URLs, as well as options.\nSee -help for more information.")
 	}
 
-	log.Fatal("Please specify either a URL or a file containing URLs, as well as options.\nSee --help for more information.")
-	return Settings{}
+	return *settings
 }
